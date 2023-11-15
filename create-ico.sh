b8417d6cd50d33c6d35b88e799145d860c58e62a
Federico <dalpio23@gmail.com>
2023-11-15 23:24:15 +0100

    Initial commit

---
+++ create-ico.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+svg=$1
+ico=${2:-"favicon.ico"}
+
+size=(16 32 24 48 64 72 96 144 152 192 196 256)
+
+out=tmp
+mkdir -p "$out"
+
+echo Making bitmaps from your svg...
+
+for i in ${size[@]}; do
+  inkscape $svg --export-type=png -o "$out/$i.png" -w $i -h $i
+done
+
+echo Compressing...
+
+## Replace with your favorite (e.g. pngquant)
+# optipng -o7 "$out/*.png"
+find "$out" -name "*.png" -exec pngquant -f --ext .png 256 {} +
+
+echo Converting to favicon.ico...
+
+convert "$out/*.png" "$ico"
+
+# Clean-up
+rm -rf "$out/"
+
+echo Done
