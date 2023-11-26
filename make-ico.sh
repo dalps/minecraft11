28cc13e110e1841260720f1b256b94a75ba7224d
Federico <dalpio23@gmail.com>
2023-11-26 19:02:07 +0100

    Update script

--- make-ico.sh
+++ make-ico.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# Credit: https://graphicdesign.stackexchange.com/a/110023
 
 set -e
 
 svg=$1
-filename=${svg%.*}
-ico=${2:-"$filename.ico"}
-echo "$filename"
-echo "$ico"
+name=${svg%.*}
+ico=${2:-"$name.ico"}
 
 size=(16 32 24 48 64 72 96 144 152 192 196 256)
 
@@ -21,8 +20,6 @@ done
 
 echo Compressing...
 
-## Replace with your favorite (e.g. pngquant)
-# optipng -o7 "$out/*.png"
 find "$out" -name "*.png" -exec pngquant -f --ext .png 256 {} +
 
 echo Converting to $ico...
